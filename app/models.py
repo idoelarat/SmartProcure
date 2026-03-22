9f2b43ae77cb107985aa657fa0dc78e0e4b8838e
Ido <elaratido@gmail.com>
2026-03-22 18:59:43 +0200

    1. file name on top 2. added nullable args where needed

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+# models.py
 from typing import List
 from typing import Optional
 from sqlalchemy import ForeignKey
@@ -17,13 +18,13 @@ class Part(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     sku: Mapped[str] = mapped_column(String(70) , unique=True, index=True, nullable=False)
-    vendor_sku: Mapped[Optional[str]] = mapped_column(String(70), index=True)
+    vendor_sku: Mapped[Optional[str]] = mapped_column(String(70), index=True, nullable=True)
     name: Mapped[str] = mapped_column(String(100),nullable=False)
-    description: Mapped[str] = mapped_column(String(255))
+    description: Mapped[str] = mapped_column(String(255), nullable=True)
     category_id: Mapped[int] = mapped_column(ForeignKey("categories.id"))
     category: Mapped["Category"] = relationship(back_populates="parts")
 
-    def __repr__(self) -> str:
+    def __repr__(self):
         return f"Part(sku={self.sku!r}, name={self.name!r})"
 
 
@@ -34,3 +35,5 @@ class Category(Base):
     name: Mapped[str] = mapped_column(String(50), unique=True, index=True, nullable=False)
     parts: Mapped[List["Part"]] = relationship(back_populates="category")
 
+    def __repr__(self):
+        return f"categories(id={self.id!r}, name={self.name!r})"
